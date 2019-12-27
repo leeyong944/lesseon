c1484cc992b6af79d5fe3c2971dda5ff1bbc542f
8 <8@8-PC>
2019-12-27 16:09:51 +0900

    DB 연결 입력해서 데이터출력, 입력검색,수정

--- Lesseon/src/test20191226_2/DBsql.java
+++ Lesseon/src/test20191226_2/DBsql.java
@@ -1,7 +1,7 @@
 package test20191226_2;
 
 import java.sql.*;
-import java.util.Scanner;
+import java.util.*;
 
 //쿼리문을 모아놓은 클래스
 public class DBsql {
@@ -12,8 +12,12 @@ public class DBsql {
 	// 조회(SELECT) 결과를 저장하기 위한 변수 선언
 	ResultSet rs = null;
 	
+	public void dbConnection() {
+		con = DBConnection.makeConnection();
+	}
+	
 	// STUDENT 테이블 전체 조회 메소드
-	public void selectDB(Connection con) {
+	public void selectDB() {
 		//실행하고자 하는 쿼리문을 String 변수로 지정
 		String sql ="SELECT * FROM STUDENT";
 		
@@ -36,13 +40,13 @@ public class DBsql {
 		
 	}
 	// STUDENT 테이블에 데이터 추가
-	public void insertDB(Connection con) {
+	public void insertDB() {
 		String sql = "INSERT INTO STUDENT VALUES(9, '학생5', 11, '강원도', '남성', '010-9999-9999')";
 		
 		try {
 			pstmt = con.prepareStatement(sql);
 			int result = pstmt.executeUpdate(); // 성공하면 1 실패하면 0
-			System.out.println("insurt 결과 "+result);
+			System.out.println("insert 결과 "+result);
 			System.out.println("DB에 저장 성공!!");
 		} catch(SQLException e) {
 			e.printStackTrace();
@@ -62,7 +66,7 @@ public class DBsql {
 //		}
 //	}
 	
-	public void insertDB2(Connection con) {
+	public void insertDB2() {
 		String sql = "INSERT INTO STUDENT VALUES(?,?,?,?,?,?)";
 		try {
 			pstmt = con.prepareStatement(sql);
@@ -78,35 +82,35 @@ public class DBsql {
 		}
 	}
 	
-	public void insertDB1(Connection con) {
+	public void insertDB1(Student student) {
 		String sql = "INSERT INTO STUDENT VALUES(?,?,?,?,?,?)";
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 		try {
 			pstmt = con.prepareStatement(sql);
-			int studentno;
-			String name;
-			int age;
-			String address;
-			String gender;
-			String phone;
-			System.out.print("학생번호 : ");
-			studentno = scan.nextInt();
-			System.out.print("이름 : ");
-			name = scan.next();
-			System.out.print("나이 : ");
-			age = scan.nextInt();
-			System.out.print("주소 : ");
-			address = scan.next();
-			System.out.print("성별 : ");
-			gender = scan.next();
-			System.out.print("전화번호 : ");
-			phone = scan.next();
-			pstmt.setInt(1, studentno);
-			pstmt.setString(2, name);
-			pstmt.setInt(3, age);
-			pstmt.setString(4, address);
-			pstmt.setString(5, gender);
-			pstmt.setString(6, phone);
+//			int studentno;
+//			String name;
+//			int age;
+//			String address;
+//			String gender;
+//			String phone;
+//			System.out.print("학생번호 : ");
+//			studentno = scan.nextInt();
+//			System.out.print("이름 : ");
+//			name = scan.next();
+//			System.out.print("나이 : ");
+//			age = scan.nextInt();
+//			System.out.print("주소 : ");
+//			address = scan.next();
+//			System.out.print("성별 : ");
+//			gender = scan.next();
+//			System.out.print("전화번호 : ");
+//			phone = scan.next();
+			pstmt.setInt(1, student.getStudentNo());
+			pstmt.setString(2, student.getName());
+			pstmt.setInt(3, student.getAge());
+			pstmt.setString(4, student.getAddress());
+			pstmt.setString(5, student.getGender());
+			pstmt.setString(6, student.getPhone());
 			System.out.println("DB저장 완료");
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -114,8 +118,119 @@ public class DBsql {
 		}
 	}
 	
-	public void updateDB(Connection con) {
-		String sql = "UPDATE STUDENT SET AGE";
+	public List<Student> selectDB1() {
+		String sql ="SELECT * FROM STUDENT";
+		Student stu = null;
+		List<Student> list = new ArrayList<Student>();
+		try {	
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				stu = new Student(rs.getInt("studentno"),rs.getString("name"),rs.getInt("age"),
+								  rs.getString("address"),rs.getString("gender"),rs.getString("phone"));
+				list.add(stu);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+	
+	/*
+	 * 조건검색
+	 *  1. 여자인 학생만 출력하기
+	 *  2. 주소가 인천인 학생만 출력하기
+	 *  3. 주소 입력해서 출력하기
+	 */
+	
+	
+	public void condition() {
+		String sql = "SELECT * FROM STUDENT WHERE GENDER = '여성'";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				System.out.print(rs.getInt("studentNo")+", ");
+				System.out.print(rs.getString("name")+", ");
+				System.out.print(rs.getInt("age")+", ");
+				System.out.print(rs.getString("address")+", ");
+				System.out.print(rs.getString("gender")+", ");
+				System.out.println(rs.getString("phone"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void condition2() {
+		String sql = "SELECT * FROM STUDENT WHERE ADDRESS LIKE '%인천%'";
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				System.out.print(rs.getInt("studentNo")+", ");
+				System.out.print(rs.getString("name")+", ");
+				System.out.print(rs.getInt("age")+", ");
+				System.out.print(rs.getString("address")+", ");
+				System.out.print(rs.getString("gender")+", ");
+				System.out.println(rs.getString("phone"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void condition3(String city) {
+		String sql = "SELECT * FROM STUDENT WHERE ADDRESS LIKE ?";
+		List<Student> stuList = new ArrayList<Student>();
+		try {
+			Student stu = null;
+			Scanner scan = new Scanner(System.in);
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, "%"+city+"%");
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				stu = new Student();
+				stu.setStudentNo(rs.getInt("studentNo"));
+				stu.setName(rs.getString("name"));
+				stu.setAge(rs.getInt("studentNo"));
+				stu.setAddress(rs.getString("address"));
+				stu.setGender(rs.getString("gender"));
+				stu.setPhone(rs.getString("phone"));
+				stuList.add(stu);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		for(Student a : stuList) {
+			System.out.println(a);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	public void updateDB(String phone, int studentNo) {
+		String sql = "UPDATE STUDENT SET PHONE = ? WHERE STUDENTNO = ?";
+		Student stu = null;
+		try {
+			stu = new Student();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, phone);
+			pstmt.setInt(2, studentNo);
+			pstmt.executeUpdate();
+			System.out.println("수정 성공");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
--- Lesseon/src/test20191226_2/JdbcMain.java
+++ Lesseon/src/test20191226_2/JdbcMain.java
@@ -1,19 +1,20 @@
 package test20191226_2;
 
-import java.sql.Connection;
-import java.util.Scanner;
+import java.util.*;
 
 public class JdbcMain {
 
 	public static void main(String[] args) {
+		Student student = null;
+		List<Student> list = new ArrayList<Student>();
 		/*
 		 * 1. DBConnection 클래스의 makeConnection 메소드 호출하여
 		 *    DB 접속 정보를 가져옴
 		 * 2. con 객체를 가지고 DBsql 클래스의 메소드 호출하여 원하는
 		 *    쿼리문 실행
 		 */
-		Connection con = null;
-		con = DBConnection.makeConnection();
+//		Connection con = null;
+//		con = DBConnection.makeConnection();
 		
 		DBsql sql = new DBsql();
 //		sql.selectDB(con);
@@ -23,25 +24,89 @@ public class JdbcMain {
 		 */
 		Scanner scan = new Scanner(System.in);
 		int selectNum = 0;
-		while(true) {
-			System.out.println("1.전체 조회, 2. 데이터 추가, 3.데이터 추가(? 사용), 4.데이터 추가(입력)");
+		boolean run = true;
+		while(run) {
+			System.out.println("0.DB 접속, 1.전체 조회, 2. 데이터 추가, 3.데이터 추가(? 사용), 4.데이터 추가(입력), 5.전체 조회(ArrayList)"
+					 		   +", 6.조건 검색, 7.수정,  8.종료");
 			selectNum = scan.nextInt();
 			switch(selectNum) {
+			case 0 :
+				sql.dbConnection();
+				break;
 			case 1 :
-				sql.selectDB(con);
+				sql.selectDB();
 				break;
 			case 2 :
-				sql.insertDB(con);
+				sql.insertDB();
 				break;
 			case 3 :
-				sql.insertDB2(con);
+				sql.insertDB2();
 				break;
 			case 4 :
-				sql.insertDB1(con);
+				int studentNo;
+				String name;
+				int age;
+				String address;
+				String gender;
+				String phone;
+				System.out.print("학생번호 : ");
+				studentNo = scan.nextInt();
+//				student.setStudentNo(studentNo); 이런식으로 해도됨
+				System.out.print("이름 : ");
+				name = scan.next();
+				System.out.print("나이 : ");
+				age = scan.nextInt();
+				System.out.print("주소 : ");
+				address = scan.next();
+				System.out.print("성별 : ");
+				gender = scan.next();
+				System.out.print("전화번호 : ");
+				phone = scan.next();
+				student = new Student(studentNo,name,age,address,gender,phone);
+				sql.insertDB1(student);
 				break;
+			case 5 :
+//				list = new ArrayList<Student>();
+				list = sql.selectDB1();
+				//전체조회메소드를 호출하여 호출 결과를 가져옴
+				break;
+			case 6 :
+				System.out.println("1.여성만 출력, 2.인천인 학생 출력 3.주소 입력 출력");
+				int selectNum1 = scan.nextInt();
+				switch(selectNum1) {
+				case 1 : 
+					sql.condition();
+					break;
+				case 2 :
+					sql.condition2();
+					break;
+				case 3 :
+					System.out.print("검색할 주소를 입력하세요 : ");
+					String city = scan.next();
+					sql.condition3(city);
+					break;
+				}
+				
+				break;
+			case 7 :
+				System.out.print("수정할 학생 번호를 입력하세요 : ");
+				int studentNo1 = scan.nextInt();
+				System.out.print("수정할 전화 번호를 입력하세요 : ");
+				String phone1 = scan.next();
+				sql.updateDB(phone1, studentNo1);
+				break;
+			case 8 :
+				run = false;				
+				break;
+				
 			}
 		}
-		
+		int index = 0;
+		for(Student a:list) {
+			System.out.println(index+""+a);
+			index++;
+		}
+			//DB에서 조회한 전체 학생 목록을 여기 위치에서 출력
 	}
 
 }
